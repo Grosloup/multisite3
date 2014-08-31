68f6bb8d63e095f9918ced2956fe4ad19c82c4c3
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-08-31 14:01:23 +0200

    sauvegarde

---
+++ src/ZPB/AdminBundle/Controller/General/AdminUserController.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nicolas Canfrere
+ * Date: 31/08/2014
+ * Time: 13:47
+ */
+ /*
+           ____________________
+  __      /     ______         \
+ {  \ ___/___ /       }         \
+  {  /       / #      }          |
+   {/ ô ô  ;       __}           |
+   /          \__}    /  \       /\
+<=(_    __<==/  |    /\___\     |  \
+   (_ _(    |   |   |  |   |   /    #
+    (_ (_   |   |   |  |   |   |
+      (__<  |mm_|mm_|  |mm_|mm_|
+*/
+
+namespace ZPB\AdminBundle\Controller\General;
+
+
+use Symfony\Component\HttpFoundation\Request;
+use ZPB\AdminBundle\Controller\BaseController;
+
+class AdminUserController extends BaseController
+{
+    public function listAction()
+    {
+        return $this->render('ZPBAdminBundle:General/admin_user:list.html.twig', []);
+    }
+
+    public function updateAction($id, Request $request)
+    {
+
+    }
+
+    public function createAction($id, Request $request)
+    {
+
+    }
+
+    public function deleteAction($id, Request $request)
+    {
+
+    }
+}
