136d79f9bcaf0230851a11605bd142ba66d4301d
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-09-02 01:51:08 +0200

    sauvegarde

---
+++ src/ZPB/AdminBundle/Controller/Parrainage/SponsorShipController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nicolas Canfrere
+ * Date: 02/09/2014
+ * Time: 00:09
+ */
+ /*
+           ____________________
+  __      /     ______         \
+ {  \ ___/___ /       }         \
+  {  /       / #      }          |
+   {/ ô ô  ;       __}           |
+   /          \__}    /  \       /\
+<=(_    __<==/  |    /\___\     |  \
+   (_ _(    |   |   |  |   |   /    #
+    (_ (_   |   |   |  |   |   |
+      (__<  |mm_|mm_|  |mm_|mm_|
+*/
+
+namespace ZPB\AdminBundle\Controller\Parrainage;
+
+
+use ZPB\AdminBundle\Controller\BaseController;
+
+class SponsorShipController extends BaseController
+{
+
+}
