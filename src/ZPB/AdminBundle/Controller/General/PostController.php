1b5a6cb2dd4073e2cf1c33ae40f17bc66a87e7e3
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-09-06 15:17:33 +0200

    sauvegarde

--- src/ZPB/AdminBundle/Controller/General/PostController.php
+++ src/ZPB/AdminBundle/Controller/General/PostController.php
@@ -23,18 +23,28 @@ namespace ZPB\AdminBundle\Controller\General;
 
 use Symfony\Component\HttpFoundation\Request;
 use ZPB\AdminBundle\Controller\BaseController;
+use ZPB\AdminBundle\Entity\Post;
+use ZPB\AdminBundle\Form\Type\PostType;
 
 class PostController extends BaseController
 {
     public function listAction()
     {
-        return $this->render('ZPBAdminBundle:General:post/list.html.twig', []);
+        $entities = $this->getRepo('ZPBAdminBundle:Post')->findBy([], ['createdAt'=>'DESC']);
+        return $this->render('ZPBAdminBundle:General:post/list.html.twig', ['posts'=>$entities]);
     }
 
     public function createAction(Request $request)
     {
-        //TODO
-        return $this->render('ZPBAdminBundle:General:post/create.html.twig', []);
+        $entity = new Post();
+        $form = $this->createForm(new PostType(), $entity);
+        $form->handleRequest($request);
+        if($form->isValid()){
+
+            $this->setSuccess('Nouvel article d\'actualité bien créé.');
+            return $this->redirect($this->generateUrl('zpb_admin_posts_list'));
+        }
+        return $this->render('ZPBAdminBundle:General:post/create.html.twig', ['form'=>$form->createView()]);
     }
 
     public function updateAction($id, Request $request)
