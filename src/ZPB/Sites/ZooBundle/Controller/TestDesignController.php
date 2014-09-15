1966032a2e7e956abc03d7dd0630b257712dbea0
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-09-15 17:29:33 +0200

    sauvegarde

---
+++ src/ZPB/Sites/ZooBundle/Controller/TestDesignController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nicolas Canfrère
+ * Date: 15/09/2014
+ * Time: 17:05
+ */
+  /*
+           ____________________
+  __      /     ______         \
+ {  \ ___/___ /       }         \
+  {  /       / #      }          |
+   {/ ô ô  ;       __}           |
+   /          \__}    /  \       /\
+<=(_    __<==/  |    /\___\     |  \
+   (_ _(    |   |   |  |   |   /    #
+    (_ (_   |   |   |  |   |   |
+      (__<  |mm_|mm_|  |mm_|mm_|
+*/
+
+namespace ZPB\Sites\ZooBundle\Controller;
+
+
+use ZPB\AdminBundle\Controller\BaseController;
+
+class TestDesignController extends BaseController
+{
+    public function indexAction()
+    {
+        return $this->render('ZPBSitesZooBundle:TestDesign:index.html.twig', []);
+    }
+} 
