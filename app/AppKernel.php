df796071222589099b4d32abba15b74e2da86769
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-09-22 19:55:41 +0200

    sauvegarde

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
+            new SunCat\MobileDetectBundle\MobileDetectBundle(),
+
             new ZPB\AdminBundle\ZPBAdminBundle(),
             new ZPB\Sites\ZooBundle\ZPBSitesZooBundle(),
             new ZPB\Sites\BNBundle\ZPBSitesBNBundle(),
