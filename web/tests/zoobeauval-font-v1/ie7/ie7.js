214d325a635edabaa1599929116093bd136aaa40
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-10-01 12:24:59 +0200

    sauvegarde

---
+++ web/tests/zoobeauval-font-v1/ie7/ie7.js
@@ -0,0 +1,35 @@
+/* To avoid CSS expressions while still supporting IE 7 and IE 6, use this script */
+/* The script tag referring to this file must be placed before the ending body tag. */
+
+/* Use conditional comments in order to target IE 7 and older:
+	<!--[if lt IE 8]><!-->
+	<script src="ie7/ie7.js"></script>
+	<!--<![endif]-->
+*/
+
+(function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'zoobeauval\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+		'icon-cart-o': '&#xe600;',
+		'icon-cart': '&#xe601;',
+		'icon-account-o': '&#xe602;',
+		'icon-account': '&#xe603;',
+		'0': 0
+		},
+		els = document.getElementsByTagName('*'),
+		i, c, el;
+	for (i = 0; ; i += 1) {
+		el = els[i];
+		if(!el) {
+			break;
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+}());
