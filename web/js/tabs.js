2090d1f1498a0bea0e70089cf5a14c702db427f2
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-10-08 15:49:52 +0200

    sauvegarde

--- web/js/tabs.js
+++ web/js/tabs.js
@@ -1,8 +1,26 @@
 (function($, w){
-    $.fn.tabs = function(){
-
+    var tabs_default = {
+        btnSelector : '._tab',
+        panelSelector: '._tab-panel',
+        activeClass: 'active'
+    };
+    $.fn.tabs = function(o){
+        var opts = $.extend({}, tabs_default, o || {});
         return this.each(function(){
+            var $el = $(this);
+            var $tabs = $el.find(opts.btnSelector);
+            var panels = $(opts.panelSelector);
 
+            $tabs.each(function(){
+                var target = $($(this).data("target"));
+                $(this).on("click", function(e){
+                    e.preventDefault();
+                    panels.removeClass(opts.activeClass);
+                    target.addClass(opts.activeClass);
+                    $tabs.removeClass("active");
+                    $(this).addClass("active");
+                });
+            });
         });
     }
 })(jQuery, this);
