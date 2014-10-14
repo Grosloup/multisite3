ad6489ce654f122b6453734a53dc40e22516e586
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-10-14 12:01:50 +0200

    sauvegarde

--- src/ZPB/AdminBundle/Controller/General/PressKitController.php
+++ src/ZPB/AdminBundle/Controller/General/PressKitController.php
@@ -21,6 +21,8 @@
 namespace ZPB\AdminBundle\Controller\General;
 
 
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use ZPB\AdminBundle\Controller\BaseController;
 use ZPB\AdminBundle\Entity\PressKit;
@@ -54,7 +56,15 @@ class PressKitController extends BaseController
         if(!$entity){
             throw $this->createNotFoundException();
         }
-        return $this->render('ZPBAdminBundle:General/PressKit:update.html.twig', []);
+        $form = $this->createForm(new PressKitType(), $entity, ['em'=>$this->getManager()]);
+        $form->handleRequest($request);
+        if($form->isValid()){
+            $this->getManager()->persist($entity);
+            $this->getManager()->flush();
+            $this->setSuccess('Nouveau dossier bien modifié');
+            return $this->redirect($this->generateUrl('zpb_admin_general_press_kit_list'));
+        }
+        return $this->render('ZPBAdminBundle:General/PressKit:update.html.twig', ['form'=>$form->createView()]);
     }
 
     public function deleteAction($id,Request $request)
@@ -65,15 +75,112 @@ class PressKitController extends BaseController
         if(null == $entity = $this->getRepo('ZPBAdminBundle:PressKit')->find($id)){
             throw $this->createNotFoundException();
         }
+        $this->getManager()->remove($entity);
+        $this->getManager()->flush();
+        $this->setSuccess('Dossier bien supprimé');
+        return $this->redirect($this->generateUrl('zpb_admin_general_press_kit_list'));
     }
 
     public function uploadPdfAction(Request $request)
     {
+        if(!$request->isMethod("POST") || !$request->isXmlHttpRequest()){
+            throw $this->createAccessDeniedException();
+        }
+        $fs = $this->get('filesystem');
+        $filename = $request->headers->get('X-File-Name');
+        $fileType = $request->headers->get('X-File-Type', false);
+        $response = ['error'=>false,'msg'=>'', 'pdfId'=>''];
+        if(!$filename || !$fileType){
+            $response = ['error'=>true,'msg'=>'Données manquantes', 'pdfId'=>''];
+        } elseif(!in_array($fileType, ['application/pdf'])){
+            $response = ['error'=>true,'msg'=>'Données érronées', 'imgId'=>''];
+        } else {
+            $basePath = $this->container->getParameter('zpb.medias.options')['zpb.pdf.root_dir'] . $this->container->getParameter('zpb.medias.options')['zpb.pdf.web_dir'];
+            if(!$fs->exists($basePath)){
+                $fs->mkdir($basePath);
+            }
+            $lang = $request->headers->get('X-File-Lang');
+            if($lang && $lang != 'fr'){
+                $lang .= '_';
+            } else {
+                $lang = '';
+            }
+            $path = $basePath . $lang . $filename;
+            if($fs->exists($path)){
 
+                $filenameNoExtension = preg_replace('/\.pdf$/i','',$filename);
+                $old = $this->getRepo('ZPBAdminBundle:MediaPdf')->findOneByFilename($filenameNoExtension);
+                if($old){
+                    $this->getManager()->remove($old);
+                    $this->getManager()->flush();
+                }
+            }
+            file_put_contents($path, $request->getContent());
+            $file = new File($path);
+            $pdf = $this->get('zpb.pdf_factory')->createFromFile($file);
+            if(null != $institutionId = $request->headers->get('X-File-Institution', null)){
+                $institution = $this->getRepo('ZPBAdminBundle:Institution')->find($institutionId);
+                if($institution){
+                    $pdf->setInstitution($institution);
+                }
+            }
+            try{
+                $this->getManager()->persist($pdf);
+                $this->getManager()->flush();
+                $response = ['error'=>false,'msg'=>'Transfert réussi', 'pdfId'=>$pdf->getId()];
+            } catch(\Exception $e){
+                $response = ['error'=>true,'msg'=>$e->getMessage(), 'pdfId'=>''];
+            }
+        }
+
+
+        return new JsonResponse($response);
     }
 
     public function uploadImageAction(Request $request)
     {
+        if(!$request->isMethod("POST") || !$request->isXmlHttpRequest()){
+            throw $this->createAccessDeniedException();
+        }
+        $fs = $this->get('filesystem');
+        $filename = $request->headers->get('X-File-Name', false);
+        $fileType = $request->headers->get('X-File-Type', false);
+        $response = ['error'=>false,'msg'=>'', 'imgId'=>''];
 
+        if(!$filename || !$fileType){
+            $response = ['error'=>true,'msg'=>'Données manquantes', 'imgId'=>''];
+        } elseif(!in_array($fileType, ['image/jpeg', 'image/gif', 'image/png'])){
+            $response = ['error'=>true,'msg'=>'Données érronées', 'imgId'=>''];
+        } else {
+            $basePath =
+                $this->container->getParameter('zpb.medias.options')['zpb.img.root_dir']
+                . $this->container->getParameter('zpb.medias.options')['zpb.img.web_dir']
+            ;
+            if($fs->exists($basePath)){
+                $fs->mkdir($basePath);
+            }
+            $path = $basePath . $filename;
+            if($fs->exists($path)){
+                $filenameNoExtension = pathinfo($filename, PATHINFO_FILENAME);
+                $old = $this->getRepo('ZPBAdminBundle:MediaImage')->findOneByFilename($filenameNoExtension);
+                if($old){
+                    $this->getManager()->remove($old);
+                    $this->getManager()->flush();
+                }
+            }
+            file_put_contents($path, $request->getContent());
+            $file = new File($path);
+            $image = $this->get('zpb.image_factory')->createFromFile($file);
+            $this->get('zpb.image_resizer')->makeThumbnails($image);
+            try{
+                $this->getManager()->persist($image);
+                $this->getManager()->flush();
+                $response = ['error'=>false,'msg'=>'Transfert réussi', 'imageId'=>$image->getId()];
+            } catch(\Exception $e){
+                $response = ['error'=>true,'msg'=>$e->getMessage(), 'imageId'=>''];
+            }
+        }
+
+        return new JsonResponse($response);
     }
 }
