73ee57f46edc1f2a13b08224b6c29b890726c891
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-10-15 15:27:48 +0200

    sauvegarde

--- src/ZPB/AdminBundle/Controller/General/PostController.php
+++ src/ZPB/AdminBundle/Controller/General/PostController.php
@@ -21,6 +21,8 @@
 namespace ZPB\AdminBundle\Controller\General;
 
 
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use ZPB\AdminBundle\Controller\BaseController;
 use ZPB\AdminBundle\Entity\Post;
@@ -84,6 +86,53 @@ class PostController extends BaseController
             return $this->redirect($this->generateUrl('zpb_admin_posts_list'));
         }
         return $this->render('ZPBAdminBundle:General:post/create.html.twig', ['form'=>$form->createView()]);
+    }
+
+    public function uploadImageAction(Request $request)
+    {
+        if(!$request->isMethod("POST") || !$request->isXmlHttpRequest()){
+            throw $this->createAccessDeniedException();
+        }
+        $fs = $this->get('filesystem');
+        $filename = $request->headers->get('X-File-Name', false);
+        $fileType = $request->headers->get('X-File-Type', false);
+        $response = ['error'=>false,'msg'=>'', 'imgId'=>''];
+
+        if(!$filename || !$fileType){
+            $response = ['error'=>true,'msg'=>'Données manquantes', 'imgId'=>''];
+        } elseif(!in_array($fileType, ['image/jpeg', 'image/gif', 'image/png'])){
+            $response = ['error'=>true,'msg'=>'Données érronées', 'imgId'=>''];
+        } else {
+            $basePath =
+                $this->container->getParameter('zpb.medias.options')['zpb.img.root_dir']
+                . $this->container->getParameter('zpb.medias.options')['zpb.img.web_dir']
+            ;
+            if($fs->exists($basePath)){
+                $fs->mkdir($basePath);
+            }
+            $path = $basePath . $filename;
+            if($fs->exists($path)){
+                $filenameNoExtension = pathinfo($filename, PATHINFO_FILENAME);
+                $old = $this->getRepo('ZPBAdminBundle:MediaImage')->findOneByFilename($filenameNoExtension);
+                if($old){
+                    $this->getManager()->remove($old);
+                    $this->getManager()->flush();
+                }
+            }
+            file_put_contents($path, $request->getContent());
+            $file = new File($path);
+            $image = $this->get('zpb.image_factory')->createFromFile($file);
+            $this->get('zpb.image_resizer')->makeThumbnails($image);
+            try{
+                $this->getManager()->persist($image);
+                $this->getManager()->flush();
+                $response = ['error'=>false,'msg'=>'Transfert réussi', 'imageId'=>$image->getId()];
+            } catch(\Exception $e){
+                $response = ['error'=>true,'msg'=>$e->getMessage(), 'imageId'=>''];
+            }
+        }
+
+        return new JsonResponse($response);
     }
 
     public function updateAction($id, Request $request)
--- web/js/admin/newPost.js
+++ web/js/admin/newPost.js
@@ -1,227 +1,141 @@
-(function($,w,d){
-    jQuery.event.props.push( "dataTransfer" );
-    var uploadOptions = {
-        multiple: false,
-        droparea: '.dropzone-in',
-        droploader: '.dropzone-loader',
-        progressbar: '.dropzone-innerbar',
-        actions: '.dropzone-actions',
-        message: '.dropzone-message',
-        deleteBtn: '.dropzone-delete-btn',
+(function($,w){
+    var imageDefaults = {
         hover: 'hover',
-        legend: 'Déposez votre image ici',
+        progressBar: '.dropzone-innerbar',
+        url: null,
+        maxSize: 1000000,
         allow: ['image/jpeg', 'image/gif', 'image/png'],
-        maxSize: 4500000,
-        url: '/xhr/upload/img',
-        deleteImgUrl: '/xhr/supprimer/img'
+        errorMessage: function(message){},
+        loadDone: function(response){},
+        loadFail: function(response){}
+    };
+
+    function ImageUploader(el, options){
+        this.$el = $(el);
+        this.options = $.extend({}, imageDefaults, options || {});
+        this.init();
+    }
+
+    ImageUploader.prototype.init = function(){
+        var el = this.$el;
+        var self = this;
+        this.isDroppable = true;
+
+        this.progressBar = el.find(this.options.progressBar);
+        el.on({
+            dragenter: self.dragenter,
+            dragover: self.dragover,
+            dragleave: self.dragleave,
+            drop: self.drop
+        });
+    };
+
+
+    ImageUploader.prototype.dragenter = function(e){
+        e.preventDefault();
+    };
+
+    ImageUploader.prototype.dragover = function(e){
+        e.preventDefault();
+        var self = $(this).data("zpbUploadImage");
+        $(this).addClass(self.options.hover);
+    };
+
+    ImageUploader.prototype.dragleave = function(e){
+        e.preventDefault();
+        var self = $(this).data("zpbUploadImage");
+        $(this).removeClass(self.options.hover);
+    };
+
+    ImageUploader.prototype.drop = function(e){
+        e.preventDefault();
+        var self = $(this).data("zpbUploadImage");
+        $(this).removeClass(self.options.hover);
+        self.options.errorMessage('');
+        if(self.isDroppable === true){
+            self.isDroppable = false;
+            self.upload(e.dataTransfer.files);
+        }
+    };
+
+    ImageUploader.prototype.upload = function(files){
+        var file = files[0];
+        var self = this;
+        if(!'size' in file){
+            this.options.errorMessage('');
+            this.options.errorMessage('L\'élément déposé n\'est pas un fichier');
+            return false;
+        }
+        if(this.options.allow.indexOf(file.type)<0){
+            this.options.errorMessage('');
+            this.options.errorMessage('L\'élément déposé n\'est pas du bon type ' + file.type);
+            return false;
+        }
+        if(file.size > this.options.maxSize){
+            this.options.errorMessage('');
+            this.options.errorMessage('L\'élément déposé est trop lourd');
+            return false;
+        }
+
+        function load(e){
+            var response = $.parseJSON(e.target.responseText);
+            self.isDroppable = true;
+            if(!response.error){
+                self.options.loadDone(response);
+            } else {
+                self.options.loadFail(response);
+            }
+        }
+
+        function progress(e){
+            if(e.lengthComputable){
+                var percent = Math.round((e.loaded/ e.total) * 100) + "%";
+                self.progressBar.width(percent);
+            }
+        }
+
+        this.xhr = new XMLHttpRequest();
+
+        this.xhr.upload.addEventListener("progress", progress, false);
+
+        this.xhr.open('post', this.options.url, true);
+
+        this.xhr.onreadystatechange = function (e) {
+            if(self.xhr.readyState == 4){
+                self.isDroppable = true;
+                if(self.xhr.status == 200){
+                    var response = $.parseJSON(e.target.responseText);
+                    if(!response.error){
+                        self.options.loadDone(response);
+                    } else {
+                        self.options.loadFail(response);
+                    }
+                } else {
+                    self.options.errorMessage('');
+                    self.options.errorMessage(self.xhr.status + " " + self.xhr.statusText);
+                }
+            }
+        };
+        this.xhr.setRequestHeader('X-Requested-With','XMLHttpRequest');
+        this.xhr.setRequestHeader('Content-Type','multipart/form-data');
+        this.xhr.setRequestHeader('X-File-Name',file.name);
+        this.xhr.setRequestHeader('X-File-Type',file.type);
+        this.xhr.setRequestHeader('X-File-Size',file.size);
+        this.xhr.send(file);
 
     };
 
-    function upload(files, zone, idx, opts){
-        var file = files[idx], message= zone.find(opts.message);
-        if(!'size' in file){
-            message.text("L'élément déposé n'est pas un fichier");
-            return false;
-        }
-        if(file.size > opts.maxSize){
-            message.text("L'élément déposé est trop lourd");
-            return false;
-        }
-        if(opts.allow.indexOf(file.type) < 0 ){
-            message.text("L'élément déposé n'est pas du bon type");
-            return false;
-        }
-        var dropin = zone.find(opts.droparea);
-        var progressBar = zone.find(opts.progressbar);
-        var actions = zone.find(opts.actions);
-        var xhr = new XMLHttpRequest();
-
-        function loadFunc(e){
-            var response = $.parseJSON(e.target.responseText);
-
-            if(response.error){
-                message.text(response.msg);
-                progressBar.width(0+"%");
-                zone.data('droppable', true);
-            } else {
-                dropin.after(response.html);
-                dropin.hide();
-                message.text(response.msg);
-                progressBar.width(0+"%");
-                progressBar.parent().hide();
-                actions.removeClass("hide").addClass('show');
-                zone.data('droppable', false);
-            }
 
 
-        }
-
-        function progressFunc(e){
-            if(e.lengthComputable){
-                var percent = Math.round((e.loaded/ e.total) * 100) + "%";
-                progressBar.width(percent);
-            }
-        }
-
-        xhr.addEventListener("load", loadFunc, false);
-        xhr.upload.addEventListener("progress", progressFunc, false);
-
-        xhr.open('post', opts.url, true);
-
-        xhr.setRequestHeader('X-Requested-With','XMLHttpRequest');
-        xhr.setRequestHeader('Content-Type','multipart/form-data');
-        xhr.setRequestHeader('X-File-Name',file.name);
-        xhr.setRequestHeader('X-File-Type',file.type);
-        xhr.setRequestHeader('X-File-Size',file.size);
-
-        xhr.setRequestHeader('X-File-Id',zone.data('longid'));
-
-        xhr.send(file);
-    }
-
-    $.fn.uploadFile = function(options){
-        var opts = $.extend({}, uploadOptions, options || {});
-
-        this.each(function(){
-            var $this = $(this), legend = $this.find(".dropzone-legend");
-            var deleteBtn = $this.find(opts.deleteBtn);
-            var progressBar = $this.find(opts.progressbar);
-            var message = $this.find(opts.message);
-            var actions = $this.find(opts.actions);
-            var dropin = $this.find(opts.droparea);
-            legend.text(opts.legend);
-            $this.data('droppable', true);
-            $this.on({
-                dragenter: function(e){
-                    e.preventDefault();
-
-                },
-                dragover: function(e){
-                    e.preventDefault();
-                    $(this).addClass(opts.hover);
-                },
-                dragleave: function(e){
-                    e.preventDefault();
-                    $(this).removeClass(opts.hover);
-
-                },
-                drop: function(e){
-                    e.preventDefault();
-                    $(this).removeClass(opts.hover);
-                    if($this.data('droppable')){
-                        upload(e.dataTransfer.files, $this, 0, opts);
-                    }
-
+    $.fn.zpbUploadImage = function(options){
+        if(!options.url){
+            console.error('url non définie !');
+        } else {
+            return this.each(function(){
+                if(!$.data(this, "zpbUploadImage")){
+                    $.data(this, "zpbUploadImage", new ImageUploader(this, options));
                 }
             });
-
-            deleteBtn.on("click", function(e){
-                e.preventDefault();
-                var img = $this.find("img");
-                var id = img.data("id");
-
-                $this.data('droppable', true);
-                if(id){
-                    $.get(opts.deleteImgUrl + '/' + id).done(function(resp){
-                        if(resp.error){
-                            message.text('Une erreur est survenue !');
-                        } else {
-                            img.remove();
-                            progressBar.parent().show();
-                            message.text('');
-                            actions.removeClass("show").addClass("hide");
-                            dropin.show();
-                        }
-                    }).fail(function(){
-                        message.text('Une erreur est survenue !');
-                    });
-                }
-            });
-        });
-
-    }
-})(jQuery, this, document);
-
-
-
-$(function(){
-    $("textarea[data-editor]").each(function(){
-        var textarea = $(this);
-        var mode = textarea.data('editor') || 'html';
-        var size = textarea.data('editorsize') || 'medium';
-        var height = (size == "small") ? 250 : (size == "medium") ? 400 : (size == "large") ? 600 : 800;
-        var options = {
-            position: 'absolute',
-            width: textarea.width(),
-            height: height,
-            'class': textarea.attr('class')
-        };
-        var editorDiv = $("<div/>", options);
-        editorDiv.insertBefore(textarea);
-        textarea.css('display','none');
-        var editor = ace.edit(editorDiv[0]);
-        editor.getSession().setValue(textarea.val());
-        editor.setTheme('ace/theme/merbivore');
-        editor.getSession().setMode('ace/mode/'+mode);
-        editor.getSession().setUseWrapMode(true);
-        editor.getSession().setWrapLimitRange(80,80);
-        editor.renderer.setPrintMarginColumn(80);
-        editor.setOption('enableEmmet', true);
-        editor.getSession().on('change', function(e){
-            textarea.val(editor.getSession().getValue());
-        });
-    });
-
-
-
-
-    var addTagBtn, tagsContainer, counter;
-    addTagBtn = $("#addTagBtn");
-    tagsContainer = $("#tagsContainer");
-    counter = tagsContainer.children().length;
-    function buildTagForm(idx){
-        var template =
-             "<div class='form-row' id='post_form_tags_"+idx+"_name_parent'>"
-                +"<div class='form-field'>"
-                     +"<div class='row'>"
-                         +"<div class='column-10'>"
-                            +"<input type='text' id='post_form_tags_"+idx+"_name' name='post_form[tags]["+idx+"][name]'/>"
-                         +"</div>"
-                         +"<div class='column-2'>"
-                            +"<button class='removeTagBtn btn btn-primary' data-target='post_form_tags_"+idx+"_name_parent'><i class='fa fa-minus'></i></button>"
-                         +"</div>"
-                     +"</div>"
-                +"</div>"
-            +"</div>";
-        return $(template);
-    }
-    addTagBtn.on("click", function(e){
-        e.preventDefault();
-        tagsContainer.append(buildTagForm(counter++));
-        return false;
-    });
-    $("body").on("click", ".removeTagBtn", function(e){
-        e.preventDefault();
-        var target = $("#" + $(this).data("target")) || null;
-        if(target){
-            target.remove();
         }
-    });
-
-    var dropzone;
-    dropzone = $("#dropzone");
-    dropzone.uploadFile();
-
-
-});
-
-
-
-
-
-
-
-
-
-
-
+    }
+})(jQuery, this);
