a9d8da8492d8a0ac39c22054d06898933d05bdb4
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-10-17 18:04:04 +0200

    sauvegarde

--- src/ZPB/Sites/ZooMobileBundle/Controller/PratiqueController.php
+++ src/ZPB/Sites/ZooMobileBundle/Controller/PratiqueController.php
@@ -64,4 +64,9 @@ class PratiqueController extends BaseController
         return $this->render('ZPBSitesZooMobileBundle:Pratique:faq.html.twig', ['faqs'=>$faqs, 'cfaqs'=>$cfaqs]);
     }
 
+    public function contactAction()
+    {
+
+    }
+
 }
