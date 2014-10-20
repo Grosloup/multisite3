1064e5b7e2c08854da1c67df517015c223937d14
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-10-20 14:21:55 +0200

    sauvegarde

--- src/ZPB/Sites/ZooBundle/Controller/ActualiteController.php
+++ src/ZPB/Sites/ZooBundle/Controller/ActualiteController.php
@@ -32,9 +32,9 @@ class ActualiteController extends BaseController
         //
         // paginés
         //
-        $target = $this->getRepo('ZPBAdminBundle:PostTarget')->findOneByAcronym('zb');
+        /*$target = $this->getRepo('ZPBAdminBundle:PostTarget')->findOneByAcronym('zb');
         $posts = $this->getRepo('ZPBAdminBundle:Post')->getPublished($target);
-        return $this->render('ZPBSitesZooBundle:Actualite:index.html.twig', ['posts'=>$posts, 'img_factory'=>$this->get('zpb.image_factory')]);
+        return $this->render('ZPBSitesZooBundle:Actualite:index.html.twig', ['posts'=>$posts, 'img_factory'=>$this->get('zpb.image_factory')]);*/
     }
 
     // tri par categorie, publiés, ciblants zoo, paginés
@@ -43,61 +43,61 @@ class ActualiteController extends BaseController
 
     public function singleAction($slug)
     {
-        $post = $this->getRepo('ZPBAdminBundle:Post')->findOneBySlug($slug);
+        /*$post = $this->getRepo('ZPBAdminBundle:Post')->findOneBySlug($slug);
         if(!$post){
             throw $this->createAccessDeniedException();
         }
 
-        return $this->render('ZPBSitesZooBundle:Actualite:single.html.twig', ['post'=>$post]);
+        return $this->render('ZPBSitesZooBundle:Actualite:single.html.twig', ['post'=>$post]);*/
     }
 
     public function nouveautesAction()
     {
-        return $this->forward('ZPBSitesZooBundle:Actualite:postsByCategory', ['slug'=>'nouveaute']);
+        //return $this->forward('ZPBSitesZooBundle:Actualite:postsByCategory', ['slug'=>'nouveaute']);
     }
 
     public function carnetRoseAction()
     {
-        return $this->forward('ZPBSitesZooBundle:Actualite:postsByCategory', ['slug'=>'carnet-rose']);
+        //return $this->forward('ZPBSitesZooBundle:Actualite:postsByCategory', ['slug'=>'carnet-rose']);
     }
 
     public function postsByCategoryAction($slug)
     {
-        $category = $this->getRepo('ZPBAdminBundle:PostCategory')->findOneBySlug($slug);
+        /*$category = $this->getRepo('ZPBAdminBundle:PostCategory')->findOneBySlug($slug);
         if(!$category){
             throw $this->createNotFoundException();
         }
         $target = $this->getRepo('ZPBAdminBundle:PostTarget')->findOneByAcronym('zb');
         $posts = $this->getRepo('ZPBAdminBundle:Post')->getPublishedByCategoryAndTarget($category, $target);
 
-        return $this->render('ZPBSitesZooBundle:Actualite:post_by_category.html.twig', ['posts'=>$posts, 'category'=>$category]);
+        return $this->render('ZPBSitesZooBundle:Actualite:post_by_category.html.twig', ['posts'=>$posts, 'category'=>$category]);*/
     }
 
     public function postsByTagAction($slug)
     {
-        $tag = $this->getRepo('ZPBAdminBundle:PostTag')->findOneBySlug($slug);
+        /*$tag = $this->getRepo('ZPBAdminBundle:PostTag')->findOneBySlug($slug);
         if(!$tag){
             throw $this->createNotFoundException();
         }
         $target = $this->getRepo('ZPBAdminBundle:PostTarget')->findOneByAcronym('zb');
         $posts = $this->getRepo('ZPBAdminBundle:Post')->getPublishedByTagAndTarget($tag, $target);
 
-        return $this->render('ZPBSitesZooBundle:Actualite:posts_by_tag.html.twig', ['posts'=>$posts, 'tag'=>$tag]);
+        return $this->render('ZPBSitesZooBundle:Actualite:posts_by_tag.html.twig', ['posts'=>$posts, 'tag'=>$tag]);*/
     }
 
     public function listCategoriesAction()
     {
-        $target = $this->getRepo('ZPBAdminBundle:PostTarget')->findOneByAcronym('zb');
+        /*$target = $this->getRepo('ZPBAdminBundle:PostTarget')->findOneByAcronym('zb');
         $categories = $this->getRepo('ZPBAdminBundle:Post')->getCategoriesByTarget($target);
 
-        return $this->render('ZPBSitesZooBundle:Actualite:categories.html.twig', ['categories'=>$categories]);
+        return $this->render('ZPBSitesZooBundle:Actualite:categories.html.twig', ['categories'=>$categories]);*/
     }
 
     public function listTagsAction()
     {
-        $target = $this->getRepo('ZPBAdminBundle:PostTarget')->findOneByAcronym('zb');
+        /*$target = $this->getRepo('ZPBAdminBundle:PostTarget')->findOneByAcronym('zb');
         $tags = $this->getRepo('ZPBAdminBundle:Post')->getTagsByTarget($target);
 
-        return $this->render('ZPBSitesZooBundle:Actualite:tags.html.twig', ['tags'=>$tags]);
+        return $this->render('ZPBSitesZooBundle:Actualite:tags.html.twig', ['tags'=>$tags]);*/
     }
 }
