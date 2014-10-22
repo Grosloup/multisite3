07aca0827d59f6e2b5c98e66d73ec2d857881269
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-10-23 00:10:06 +0200

    sauvegarde

--- src/ZPB/AdminBundle/DataFixtures/ORM/LoadPosts.php
+++ src/ZPB/AdminBundle/DataFixtures/ORM/LoadPosts.php
@@ -44,17 +44,17 @@ class LoadPosts extends AbstractFixture implements OrderedFixtureInterface, Cont
     {
         $this->container = $container;
     }
-    
+
     public function load(ObjectManager $manager)
     {
         $this->em = $manager;
-        
+
         $this->loadDrafts();
 
         $this->loadPublished();
 
     }
-    
+
     public function getOrder()
     {
         return 27;
@@ -121,7 +121,17 @@ class LoadPosts extends AbstractFixture implements OrderedFixtureInterface, Cont
         $this->getReference('zpb-actu-draft-12')->setIsPublished(true);
         $this->em->persist($this->getReference('zpb-actu-draft-12'));
         $this->em->persist($pubPost6);
-        
+
+        $pubPost7 = new PublishedPost();
+        $pubPost7->setPost($this->getReference('zpb-actu-draft-14'))
+            ->setTarget('zoo')
+            ->setCategory($this->getReference('zpb-post-cat-zoo-16'))
+            ->addTag($this->getReference('zpb-post-tag-zoo-1'))->addTag($this->getReference('zpb-post-tag-zoo-3'))
+            ->setPublishedAt(new \DateTime());
+        $this->getReference('zpb-actu-draft-12')->setIsPublished(true);
+        $this->em->persist($this->getReference('zpb-actu-draft-12'));
+        $this->em->persist($pubPost7);
+
         $this->em->flush();
 
         $this->addReference('zpb-actu-pub-1', $pubPost1);
@@ -130,8 +140,9 @@ class LoadPosts extends AbstractFixture implements OrderedFixtureInterface, Cont
         $this->addReference('zpb-actu-pub-4', $pubPost4);
         $this->addReference('zpb-actu-pub-5', $pubPost5);
         $this->addReference('zpb-actu-pub-6', $pubPost6);
-        
-        
+        $this->addReference('zpb-actu-pub-7', $pubPost7);
+
+
     }
 
     private function loadDrafts()
@@ -196,6 +207,16 @@ class LoadPosts extends AbstractFixture implements OrderedFixtureInterface, Cont
             ->setExcerpt('Nulla ut lorem accumsan, rhoncus nunc a, suscipit ipsum. Suspendisse accumsan arcu magna, eu mollis sem pretium congue.');
         $this->em->persist($post12);
 
+        $post13 = new Post();
+        $post13->setTitle('Suspendisse condimentum ant')->setBody($this->getBody(5))
+            ->setExcerpt('Nulla ut lorem accumsan, rhoncus nunc a, suscipit ipsum. Suspendisse accumsan arcu magna, eu mollis sem pretium congue.');
+        $this->em->persist($post13);
+
+        $post14 = new Post();
+        $post14->setTitle('Les Bongos !')->setBody($this->getBody(6))
+            ->setExcerpt('Nulla ut lorem accumsan, rhoncus nunc a, suscipit ipsum. Suspendisse accumsan arcu magna, eu mollis sem pretium congue.');
+        $this->em->persist($post14);
+
 
         $this->em->flush();
         $this->addReference('zpb-actu-draft-1', $post1);
@@ -211,6 +232,8 @@ class LoadPosts extends AbstractFixture implements OrderedFixtureInterface, Cont
         $this->addReference('zpb-actu-draft-10', $post10);
         $this->addReference('zpb-actu-draft-11', $post11);
         $this->addReference('zpb-actu-draft-12', $post12);
+        $this->addReference('zpb-actu-draft-13', $post13);
+        $this->addReference('zpb-actu-draft-14', $post14);
     }
     private function getBody($num)
     {
@@ -220,7 +243,8 @@ class LoadPosts extends AbstractFixture implements OrderedFixtureInterface, Cont
             '<p>Phasellus congue cursus ex, vel mattis nulla maximus sit amet. Vivamus arcu neque, pellentesque vitae massa nec, pharetra luctus turpis. Ut suscipit ante id ligula facilisis dignissim. Morbi rhoncus tortor sed arcu molestie dapibus. Suspendisse at viverra dui, vel euismod risus.</p><p>Suspendisse eleifend augue id arcu blandit, in tempus dui posuere. Sed scelerisque suscipit arcu quis mollis. Curabitur vel augue vitae lacus iaculis hendrerit. Phasellus congue cursus ex, vel mattis nulla maximus sit amet. Vivamus arcu neque, pellentesque vitae massa nec, pharetra luctus turpis.</p><p>Suspendisse eleifend augue id arcu blandit, in tempus dui posuere. Sed scelerisque suscipit arcu quis mollis. Curabitur vel augue vitae lacus iaculis hendrerit. Phasellus congue cursus ex, vel mattis nulla maximus sit amet. Vivamus arcu neque, pellentesque vitae massa nec, pharetra luctus turpis.</p>',
             '<p>Phasellus eget dolor faucibus, imperdiet felis sit amet, iaculis justo. Cras sed nunc ac elit suscipit convallis at non ante. Mauris sed interdum diam. Maecenas egestas commodo augue, eu ultrices felis placerat tincidunt. Proin maximus, eros malesuada auctor lacinia, turpis tellus hendrerit ex, et tincidunt purus elit at felis.</p><p>In ornare mattis dictum. Proin purus velit, maximus in tellus nec, venenatis euismod risus. Aliquam nec finibus diam. Sed non vulputate est. Morbi vel ipsum ut ex placerat finibus eu a sapien. Aenean mattis turpis pretium massa ultrices vulputate. Cras sem urna, suscipit ac nisi eget, molestie porta orci.</p><p>Nunc hendrerit porttitor sem, in vestibulum nunc imperdiet ut. Suspendisse lobortis placerat dolor, vel facilisis elit sodales vitae. Aliquam tellus lectus, vehicula ut mauris a, viverra viverra ante. Aenean accumsan urna ut nisl luctus, in porta elit convallis. Suspendisse urna purus, mattis ac mi nec, imperdiet hendrerit odio. </p>',
             '<p>Nulla ut lorem accumsan, rhoncus nunc a, suscipit ipsum. Suspendisse accumsan arcu magna, eu mollis sem pretium congue. Pellentesque sed feugiat ipsum. Nulla pretium dictum fringilla. Phasellus fermentum tincidunt ornare. Phasellus euismod ante vel gravida molestie.</p><p>Suspendisse suscipit, diam et placerat laoreet, odio ex consectetur tortor, sit amet cursus purus quam sed quam. Duis commodo nisi justo, auctor pulvinar erat sodales hendrerit.</p><p>Suspendisse condimentum ante ut dui pellentesque, ut accumsan velit malesuada. Sed volutpat tempor tortor, quis porta arcu rhoncus a. Aenean bibendum, tellus in ullamcorper dapibus, tortor libero varius mauris, vel pharetra libero ligula at nisi. Aenean non placerat diam, in fermentum eros.</p>',
-            '<p>Ut suscipit ante id ligula facilisis dignissim. Morbi rhoncus tortor sed arcu molestie dapibus. Suspendisse at viverra dui, vel euismod risus. Morbi sapien nulla, ornare ut tempor ut, sodales vitae arcu. Proin id erat ut orci ornare bibendum sit amet ut mi. Suspendisse sollicitudin facilisis tristique. Pellentesque a enim nec odio sollicitudin faucibus. </p><p>Suspendisse suscipit, diam et placerat laoreet, odio ex consectetur tortor, sit amet cursus purus quam sed quam. Duis commodo nisi justo, auctor pulvinar erat sodales hendrerit.</p><p>Cras tortor augue, dignissim eget odio ac, tincidunt rutrum nisl. Proin nec ex justo. Quisque lobortis, neque vitae accumsan tristique, ante lacus interdum nisl, quis efficitur quam ex interdum ligula. Proin egestas felis in leo consequat, a feugiat nisl placerat.</p>'
+            '<p>Ut suscipit ante id ligula facilisis dignissim. Morbi rhoncus tortor sed arcu molestie dapibus. Suspendisse at viverra dui, vel euismod risus. Morbi sapien nulla, ornare ut tempor ut, sodales vitae arcu. Proin id erat ut orci ornare bibendum sit amet ut mi. Suspendisse sollicitudin facilisis tristique. Pellentesque a enim nec odio sollicitudin faucibus. </p><p>Suspendisse suscipit, diam et placerat laoreet, odio ex consectetur tortor, sit amet cursus purus quam sed quam. Duis commodo nisi justo, auctor pulvinar erat sodales hendrerit.</p><p>Cras tortor augue, dignissim eget odio ac, tincidunt rutrum nisl. Proin nec ex justo. Quisque lobortis, neque vitae accumsan tristique, ante lacus interdum nisl, quis efficitur quam ex interdum ligula. Proin egestas felis in leo consequat, a feugiat nisl placerat.</p>',
+            '<p>Ut suscipit ante id ligula facilisis dignissim. Morbi rhoncus tortor sed arcu molestie dapibus. Suspendisse at viverra dui, vel euismod risus. Morbi sapien nulla, ornare ut tempor ut, sodales vitae arcu. Proin id erat ut orci ornare bibendum sit amet ut mi. Suspendisse sollicitudin facilisis tristique. Pellentesque a enim nec odio sollicitudin faucibus. </p><p>Suspendisse suscipit, diam et placerat laoreet, odio ex consectetur tortor, sit amet cursus purus quam sed quam. Duis commodo nisi justo, auctor pulvinar erat sodales hendrerit.</p><p>Cras tortor augue, dignissim eget odio ac, tincidunt rutrum nisl. Proin nec ex justo. Quisque lobortis, neque vitae accumsan tristique, ante lacus interdum nisl, quis efficitur quam ex interdum ligula. Proin egestas felis in leo consequat, a feugiat nisl placerat.</p><p>Ut suscipit ante id ligula facilisis dignissim. Morbi rhoncus tortor sed arcu molestie dapibus. Suspendisse at viverra dui, vel euismod risus. Morbi sapien nulla, ornare ut tempor ut, sodales vitae arcu. Proin id erat ut orci ornare bibendum sit amet ut mi. Suspendisse sollicitudin facilisis tristique. Pellentesque a enim nec odio sollicitudin faucibus. </p><p>Suspendisse suscipit, diam et placerat laoreet, odio ex consectetur tortor, sit amet cursus purus quam sed quam. Duis commodo nisi justo, auctor pulvinar erat sodales hendrerit.</p><p>Cras tortor augue, dignissim eget odio ac, tincidunt rutrum nisl. Proin nec ex justo. Quisque lobortis, neque vitae accumsan tristique, ante lacus interdum nisl, quis efficitur quam ex interdum ligula. Proin egestas felis in leo consequat, a feugiat nisl placerat.</p>',
         ];
         return $body[$num];
     }
