2e6256e01ce2e95b8cfa08051df11c41093e6e9c
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-11-12 12:07:34 +0100

    sauvegarde

---
+++ web/js/sites/zoo/pedagogie/soigneur.js
@@ -0,0 +1,32 @@
+require(["jquery"], function($){
+    (function($){
+        var tabs_default = {
+            btnSelector : '._tab',
+            panelSelector: '._tab-panel',
+            activeClass: 'active'
+        };
+        $.fn.tabs = function(o){
+            var opts = $.extend({}, tabs_default, o || {});
+            return this.each(function(){
+                var $el = $(this);
+                var $tabs = $el.find(opts.btnSelector);
+                var panels = $(opts.panelSelector);
+
+                $tabs.each(function(){
+                    var target = $($(this).data("target"));
+                    $(this).on("click", function(e){
+                        e.preventDefault();
+                        panels.removeClass(opts.activeClass);
+                        target.addClass(opts.activeClass);
+                        $tabs.removeClass("active");
+                        $(this).addClass("active");
+                    });
+                });
+            });
+        }
+    })($);
+
+    $(function(){
+        $("[data-tabs]").tabs();
+    });
+});
