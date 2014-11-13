a432461d138d507fdec3a4ff37025b7a460f41ee
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-11-13 17:57:23 +0100

    sauvegarde

---
+++ src/ZPB/AdminBundle/Controller/Zoo/HeaderController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nicolas Canfrère
+ * Date: 13/11/2014
+ * Time: 17:48
+ */
+  /*
+           ____________________
+  __      /     ______         \
+ {  \ ___/___ /       }         \
+  {  /       / #      }          |
+   {/ ô ô  ;       __}           |
+   /          \__}    /  \       /\
+<=(_    __<==/  |    /\___\     |  \
+   (_ _(    |   |   |  |   |   /    #
+    (_ (_   |   |   |  |   |   |
+      (__<  |mm_|mm_|  |mm_|mm_|
+*/
+
+namespace ZPB\AdminBundle\Controller\Zoo;
+
+
+use ZPB\AdminBundle\Controller\BaseController;
+
+class HeaderController extends BaseController
+{
+
+} 
